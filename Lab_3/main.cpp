ac962daad7518ca8b0630b7f3c5eedabd68503a5
Viktor_Kornev <yoj30900@gmail.com>
2026-02-25 17:19:38 +1000

    прототип

--- Lab_3/main.cpp
+++ Lab_3/main.cpp
@@ -5,8 +5,24 @@ char foo(char c) {
 }
 
 int main() {
-    char c;
-    c = 'f';
-    std::cout << foo(c) << "samlpe";
+    int n = 10;
+    int i = 0;
+    char* s = new char[n];
+    char ch;
+    while ((ch = std::cin.get())!='\n' && ch != EOF) {
+        if (i+1 >= n) {
+            n = n*2;
+            char *current = new char[n];
+            for (int j = 0; j < n; j++) {
+                current[j] = s[j];
+            }
+            delete[] s;
+            s = current;
+        }
+        s[i++] = ch;
+
+    }
+    std::cout << s << std::endl;
+    delete[] s;
     return 0;
 }
