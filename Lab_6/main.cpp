192d9ed4222a7a35fc144435cffacf62951e7338
Viktor_Kornev <yoj30900@gmail.com>
2026-03-01 15:23:25 +1000

    Lab_5 and Lab_6 start

---
+++ Lab_6/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+class GYM {
+    public:
+    void print() {
+        std::cout << "Hello World!" << std::endl;
+    }
+
+};
+
+class b: public a {
+    public:
+
+};
+
+int main() {
+
+    return 0;
+}
